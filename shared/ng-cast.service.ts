153a10e9dbf466038203a87142c3266d7516e26f
jwjenkin <jwjenkin1@gmail.com>
2019-10-22 08:15:26 -0600

    refactor(cast-svc): convert variable functions into public/private

--- shared/ng-cast.service.ts
+++ shared/ng-cast.service.ts
@@ -7,50 +7,48 @@ import { Subject } from 'rxjs';
 export class NgCastService {
   private castSession;
   private cast;
+  private session: any;
+  private currentMedia: any;
+
   public status = {
     casting: false
   };
 
   constructor() {}
 
-  initializeCastApi() {
+  public initializeCastApi() {
     this.cast = window['chrome'].cast;
     let sessionRequest = new this.cast.SessionRequest(this.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID);
     let apiConfig = new this.cast.ApiConfig(sessionRequest,
-      s => { },
-      status => { if (status === this.cast.ReceiverAvailability.AVAILABLE) { } }
+      (s: any) => { },
+      (status: number) => {
+        if (status === this.cast.ReceiverAvailability.AVAILABLE) {
+
+        } // end if
+      }
     );
     let x = this.cast.initialize(apiConfig, this.onInitSuccess, this.onError);
   };
 
-  onInitSuccess = function (e) {
-    console.log('GCast initialization success');
-  };
-
-  onError = function (err) {
-    console.log('GCast initialization failed', err);
-  };
-
-  discoverDevices = function () {
-    let self = this;
+  public discoverDevices$() {
     const subj = new Subject();
-    this.cast.requestSession(function (s) {
-      self.session = s;
-      self.setCasting(true);
+    this.cast.requestSession((s: any) => {
+      this.session = s;
+      this.setCasting(true);
       subj.next('CONNECTED');
-    }, function (err) {
-      self.setCasting(false);
+    }, (err: any) => {
+      this.setCasting(false);
       if (err.code === 'cancel') {
-        self.session = undefined;
+        this.session = null;
         subj.next('CANCEL');
       } else {
         console.error('Error selecting a cast device', err);
       }
     });
     return subj;
-  };
+  }
 
-  launchMedia = function (media) {
+  public launchMedia(media: any) {
     let mediaInfo = new this.cast.media.MediaInfo(media);
     let request = new this.cast.media.LoadRequest(mediaInfo);
     console.log('launch media with session', this.session);
@@ -61,34 +59,42 @@ export class NgCastService {
     }
     this.session.loadMedia(request, this.onMediaDiscovered.bind(this, 'loadMedia'), this.onMediaError);
     return true;
-  };
+  }
 
-  onMediaDiscovered = function (how, media) {
-    this.currentMedia = media;
-  };
-
-  play = function () {
+  public play() {
     this.currentMedia.play(null);
   };
 
-  pause = function () {
+  public pause() {
     this.currentMedia.pause(null);
   };
 
-  stop = function () {
+  public stop() {
     this.currentMedia.stop(null);
   };
 
-  onMediaError = function (err) {
-    console.error('Error launching media', err);
-  };
-
-  setCasting(value) {
+  public setCasting(value: any) {
     this.status.casting = value;
   }
 
-  getStatus() {
+  public getStatus() {
     return this.status
   }
+
+  private onMediaDiscovered(how: any, media: any) {
+    this.currentMedia = media;
+  };
+
+  onMediaError(err) {
+    console.error('Error launching media', err);
+  };
+
+  onInitSuccess(e) {
+    console.log('GCast initialization success');
+  };
+
+  onError(err) {
+    console.log('GCast initialization failed', err);
+  };
 
 }
